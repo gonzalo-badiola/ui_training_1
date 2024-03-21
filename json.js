1db0a0c15ee0500acb8a0ac83082e7bf105ca609
Gonzalo Badiola <gonzalo.badiola@globant.com>
2024-03-21 16:56:09 -0300

    Promise

--- json.js
+++ json.js
@@ -4,30 +4,31 @@ const cart = document.getElementById("cart");
 
 
 // Datos de ejemplo de productos
-let products;
-fetch('data.json')
+let productsPromise = fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
-  .then(data => {
-    console.log(data); // Output the JSON data to the console
-    products = data
-    // Use the JSON data as needed
-  })
   .catch(error => {
     console.error('Error:', error);
+    return []; // En caso de error, devolver un array vacío
   });
+
 // Mostrar productos en la página
-function displayProducts() {
-  products.forEach((product) => {
-    const productElement = document.createElement("div");
-    productElement.classList.add("product");
-    productElement.innerHTML = `
+async function displayProducts() {
+  try {
+    const products = await productsPromise;
+    products.forEach((product) => {
+      const productElement = document.createElement("div");
+      productElement.classList.add("product");
+      productElement.innerHTML = `
             <img src="${product.image}" width=100>
             <h2>${product.title}</h2>
             <p>Precio: $${product.price}</p>
             <button onclick="addToCart(${product.id})">Agregar al carrito</button>
         `;
-    productList.appendChild(productElement);
-  });
+      productList.appendChild(productElement);
+    });
+  } catch (error) {
+    console.error('Error al mostrar productos:', error);
+  }
 }
 
 // Agregar producto al carrito
@@ -35,7 +36,9 @@ function addToCart(productId) {
   const product = products.find((p) => p.id === productId);
   if (product) {
     const cartItem = document.createElement("div");
-    cartItem.innerHTML = `<img src="${product.image}" width=100> <br> ${product.title} - $${product.price}`;
+    cartItem.setAttribute("id", product.id)
+    cartItem.innerHTML = `<button class="borrar-curso" onclick="deleteProduct(${product.id})">X</button> <img src="${product.image}" width=100> <br>
+     ${product.title} <br> Precio: $${product.price} <hr>`;
     cart.appendChild(cartItem);
   }
 }
@@ -45,12 +48,13 @@ cartButton.addEventListener("click", () => {
   cart.classList.toggle("hidden");
 });
 
-function eliminarItem(id) {
-  const itemToRemove = document.querySelector(`.cart-item[data-id="${id}"]`);
-  if (itemToRemove) {
-      itemToRemove.remove();
+function deleteProduct(id) {
+  const productToRemove = document.getElementById(`${id}`);
+  if (productToRemove) {
+    productToRemove.remove();
   }
 }
+
 function vaciarCarrito() {
   cart.innerHTML = "";
 }
